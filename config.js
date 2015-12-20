7c1d48d1769dc94a89f15d6ae169033d1afcc6c2
Fran Varney <fran@modulus.io>
2015-12-20 00:15:44 -0500

    Add mongo url option

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
 export default {
   host: process.env.HOST || 'localhost',
+  mongo: {
+    url: process.env.MONGO_URL || 'mongodb://localhost:27017/Demographics'
+  },
   port: process.env.PORT || '8080'
 }
