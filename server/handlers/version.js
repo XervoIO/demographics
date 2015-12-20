7e7c1e20a76f0acfb839a63132f0ad8f968639b0
Fran Varney <fran@modulus.io>
2015-12-20 03:22:38 -0500

    Add create versions route and handler

---
+++ server/handlers/version.js
@@ -0,0 +1,59 @@
+import Boom from 'boom'
+import Joi from 'joi'
+
+let create = {
+  validate: {
+    params: {
+      name: Joi.string().required()
+    },
+    payload: {
+      version: Joi.string().required(),
+      loc: Joi.number().integer().min(0),
+      coverage: Joi.number().min(0)
+    }
+  },
+  handler: (request, reply) => {
+    const {projects, versions} = request.server.models
+
+    projects.find({ name: request.params.name }).exec((err, foundProject) => {
+      if (err) return reply(Boom.badRequest(err))
+      if (foundProject.length < 1) return reply(Boom.badRequest('Project not found.'))
+
+      versions.find({
+        projectName: request.params.name,
+        version: request.payload.version
+      }).exec((err, foundProjectVersion) => {
+        if (err) return reply(Boom.badRequest(err))
+        if (foundProjectVersion && foundProjectVersion.version === request.payload.version)
+          return reply(Boom.badRequest('Details for that versions have been entered.'))
+
+        let {coverage, dependencies, devDependencies, loc, version} = request.payload
+        let toCreate = {
+          projectName: request.params.name,
+          version: version,
+          loc: loc,
+          coverage: coverage || 0,
+          dependencies: {
+            major: dependencies ? dependencies.major : 0,
+            minor: dependencies ? dependencies.minor : 0,
+            upToDate: dependencies ? dependencies.upToDate : 0
+          },
+          devDependencies: {
+            major: devDependencies ? devDependencies.major : 0,
+            minor: devDependencies ? devDependencies.minor : 0,
+            upToDate: devDependencies ? devDependencies.upToDate : 0
+          }
+        }
+
+        versions.create(toCreate).exec((err, newProjectVersion) => {
+          if (err) return reply(Boom.badRequest(err))
+          reply(newProjectVersion)
+        })
+      })
+    })
+  }
+}
+
+export default {
+  create: create
+}
--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,6 @@
 import Ping from './handlers/ping'
 import Project from './handlers/project'
+import Version from './handlers/version'
 
 export default [
   { method: 'GET', path: '/ping', handler: Ping },
@@ -8,5 +9,8 @@ export default [
   { method: 'POST', path: '/projects', config: Project.create },
   { method: 'DELETE', path: '/projects/{name}', config: Project.delete },
   { method: 'GET', path: '/projects/{name}', config: Project.get },
-  { method: 'GET', path: '/projects', config: Project.getAll }
+  { method: 'GET', path: '/projects', config: Project.getAll },
+
+  // Versions
+  { method: 'POST', path: '/projects/{name}/versions', config: Version.create }
 ]
