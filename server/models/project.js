c5b100bade711634dcc1f53b169bbfb181104591
Fran Varney <fran@modulus.io>
2016-01-25 14:39:54 -0500

    Add license and engines to schema

--- server/models/project.js
+++ server/models/project.js
@@ -7,8 +7,13 @@ const Project = new Mongoose.Schema({
   name: { type: String, required: true, index: true },
   maintainers: { type: Array, default: [] },
   description: { type: String },
+  hasLicense: { type: Boolean, default: false },
   hasLinter: { type: Boolean, default: false },
   hasReadme: { type: Boolean, default: false },
+  engines: {
+    npm: { type: String, default: '0.0.0' },
+    node: { type: String, default: '0.0.0' }
+  },
   versions: [{ type: Mongoose.Schema.Types.ObjectId, ref: 'Version' }]
 });
 
